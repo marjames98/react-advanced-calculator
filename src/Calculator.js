8a7b54e22495d57402e8be843a486253b59a4ac5
Amari James <mar.james1998@gmail.com>
2020-12-15 22:50:19 -0500

    notworkingyet

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,66 +1,125 @@
-import React, { Component } from 'react'
-import "./Calculator.css"
-class Calculator extends Component {
- 
-  state = {
-    num1: 0,
-    num2: 0,
-    total: null, 
-    operator: "Select One"
-  }
-  
-  setNum = (e, num) => {
-    this.setState({ [num]: parseInt(e.target.value)});
-}
+import React, { useState, useEffect } from 'react'
 
-  operator = (e) => {
-    let type = e.target.value
-    this.setState({operator: type})
-  }
+import ClearButton from './ClearButton'
+import DigitButton from './DigitButton'
+import Display from './Display'
+import EqualsButton from './EqualsButton'
+import OperatorButton from './OperatorButton'
 
-  doMath = () => {
-    let num1 = this.state.num1
-    let num2 = this.state.num2
-    let operators = this.state.operator
+const Calculator = props => {
+    const [num1, setNum1] = useState('')
+    const [operator, setOperator] = useState('')
+    const [num2, setNum2] = useState('')
+    const [tempNum, setTempNum] = useState('')
+    const [result, setResult] = useState('')
+    const [shouldCalculate, setShouldCalculate] = useState(false)
 
-    if (operators === "+") {
-      this.setState({total: num1 + num2})
+    const clear = () => {
+        setNum1('')
+        setNum2('')
+        setOperator('')
+        setTempNum('')
+        setResult('')
+        setShouldCalculate(false)
     }
-    if (operators === "-") {
-      this.setState({total: num1 - num2})
-    }
-    if (operators === "*") {
-      this.setState({total: num1 * num2})
-    }
-    if (operators === "/") {
-      this.setState({total: num1 / num2})
-    }
-    if (operators === "select") {
-      this.setState({total: "choose an operator!"})
-    }
-  }
 
-  render(){
-    return(
-      <div className="container">
-        <h1>React Calculator</h1>
- 
-        <div className="operations">
-          <input type="number" name="num1" value={this.state.num1} onChange={(e)=>this.setNum(e, 'num1')} />
-          <select onChange={this.operator}>
-            <option>Select One</option>
-            <option>+</option>
-            <option>-</option>
-            <option>*</option>
-            <option>/</option>
-          </select>
-          <input type="number" name="num2" value={this.state.num2} onChange={(e)=>this.setNum(e, 'num2')}/>
-          <button onClick={this.doMath}>=</button>
-          <h3>{this.state.total}</h3>
+    const digitEntered = (digit) => {
+       if (digit === '0' && tempNum === '') {
+           // no-op
+       } else if (digit === '.' && tempNum.includes('.')) {
+           // no-op
+       } else if (digit !== '0' && tempNum === '') {
+           setTempNum(digit)
+       } else if (tempNum !== '') {
+           setTempNum(tempNum + digit)
+       }
+    }
+
+    const operatorEntered = (op) => {
+        if (tempNum === '') {
+            // no-op
+        } else if (tempNum !== '' && num2 !== '') {
+            // no-op
+        } else if (operator !== '') {
+            // no-op
+        } else {
+            setNum1(tempNum)
+            setOperator(op)
+            setTempNum('')
+        }
+    }
+
+    const equalsEntered = () => {
+        setNum2(tempNum)
+        setTempNum('')
+        setShouldCalculate(true)
+    }
+    
+    useEffect(() => {
+        if (!shouldCalculate) { return }
+
+        const parsedNum1 = parseFloat(num1)
+        const parsedNum2 = parseFloat(num2)
+    
+        if (operator === '+') {
+            setResult(parsedNum1 + parsedNum2)
+        } else if (operator === '-') {
+            setResult(parsedNum1 - parsedNum2)
+        } else if (operator === 'x') {
+            setResult(parsedNum1 * parsedNum2)
+        } else if (operator === '/') {
+            setResult(parsedNum1 / parsedNum2)
+        }    
+    }, [shouldCalculate])
+
+    const displayForNum2 = () => {
+        if (!num1) {
+            return ''
+        } else if (!num2) {
+            return tempNum
+        } else {
+            return num2
+        }
+    }
+
+    return (
+        <div className="container">
+            <h1>React Calculator</h1>
+            <div className="calc-container">
+                <p>Values: </p>
+                <Display num1={num1 || tempNum} operator={operator} num2={displayForNum2()} result={result}/>
+                <div className="calc-row">
+                    <ClearButton clear={clear}/>
+                    <button className="calc-button calc-button-top">+/-</button>
+                    <button className="calc-button calc-button-top">%</button>
+                    <OperatorButton operator="/"operatorEntered={operatorEntered}/>
+                </div>
+                <div className="calc-row">
+                    <DigitButton digitEntered={digitEntered} digit="7"/>
+                    <DigitButton digitEntered={digitEntered} digit="8"/>
+                    <DigitButton digitEntered={digitEntered} digit="9"/>
+                    <OperatorButton operator="x" operatorEntered={operatorEntered}/>
+                </div>
+                <div className="calc-row">
+                    <DigitButton digitEntered={digitEntered} digit="4" />
+                    <DigitButton digitEntered={digitEntered} digit="5" />
+                    <DigitButton digitEntered={digitEntered} digit="6" />
+                    <OperatorButton operator="-" operatorEntered={operatorEntered}/>
+                </div>
+                <div className="calc-row">
+                    <DigitButton digitEntered={digitEntered} digit="1" />
+                    <DigitButton digitEntered={digitEntered} digit="2" />
+                    <DigitButton digitEntered={digitEntered} digit="3" />
+                    <OperatorButton operator="+" operatorEntered={operatorEntered}/>
+                </div>
+                <div className="calc-row">
+                    <DigitButton digitEntered={digitEntered} digit="0" wide />
+                    <DigitButton digitEntered={digitEntered} digit="." />
+                    <EqualsButton equalsEntered={equalsEntered}/>
+                </div>
+            </div>
         </div>
-      </div>
     )
-  }
 }
- 
-export default Calculator;
+
+export default Calculator
